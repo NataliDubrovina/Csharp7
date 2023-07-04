4c9811fbe7bdf3446bc563d7ef2488a53eb30088
Natalya <natalka118@yandex.ru>
2023-07-04 21:24:15 +0300

    Задача 2

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -7,3 +7,30 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
+Console.Write("Введите строку: ");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+int n = 3; 
+int m = 4; 
+Random random = new Random();
+int[,] arr = new int[n, m];
+Console.WriteLine("Массив: ");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+{
+    arr[i, j] = random.Next(10, 99);
+Console.Write("{0} ", arr[i, j]);
+}
+Console.WriteLine();
+}
+    if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+{
+Console.WriteLine("Элемент не существует  ");
+}
+    else
+{
+    Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+}
+Console.ReadLine();
